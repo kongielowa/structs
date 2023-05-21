bcd75d31e92e398d7489f73e4aac677451ce8ae3
kongielowa <lidiaagatakongiel@gmail.com>
2023-05-21 15:16:03 +0200

    Embedding structs (contact fields)

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	jim := person{
+		firstName: "Alex", 
+		lastName: "Patry",
+		contact: contactInfo{
+			email: "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 
 }
